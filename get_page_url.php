7e33188e045162480acd1b3385ff264720e69ce4
MD-khan <md.monir.khan707@gmail.com>
2021-01-03 13:37:03 -0500

    Update get_page_url.php

--- get_page_url.php
+++ get_page_url.php
@@ -4,6 +4,9 @@
  * So if you are on a different page use this function to retrive the url of spacific page
  */
 function md_get_the_page_url_by_title( $page_title ) {
-    $url = get_page_by_title( $page_title ) ?? "Page Name: $page_title Not found";
-    return esc_url( get_permalink( $url->ID ) );
+   $url = get_page_by_title( $page_title ) ?? false;
+    if(! $url ) {
+        return $page_title ." Not Found";
+    }
+   return esc_url( get_permalink( $url->ID ) );;
 }
